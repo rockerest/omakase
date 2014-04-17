8cf4848cb746fa59a8d712d1d4815498192e9860
rockerest <tom@vu>
2014-04-17 16:31:34 -0500

    Add a recursive merger for the routers to use

--- js/app/routers/base.js
+++ js/app/routers/base.js
@@ -2,11 +2,12 @@ define(
     [ "underscore", "objects/user" ],
     function( _, User ){
         var RouterBase = function(){
-            this.dmz = true;
-            this.blacklist = [];
-            this.whitelist = [];
-            this.user = new User();
-        };
+                this.dmz = true;
+                this.blacklist = [];
+                this.whitelist = [];
+                this.user = new User();
+            },
+            recursiveObjectMerge;
 
         RouterBase.prototype.controllerRequiresAuthentication = function(){
             return this.dmz;
@@ -43,6 +44,38 @@ define(
                 }
             }
         };
+
+        RouterBase.prototype.extend = function( one, two ){
+            return recursiveObjectMerge( recursiveObjectMerge( {}, one ), two );
+        };
+
+        /****** Helpers ******/
+        function recursiveObjectMerge( primary, overwrite ){
+            var p;
+            for( p in overwrite ){
+                try{
+                    //try an update
+                    if( overwrite[p].constructor == Object ){
+                        if (typeof primary[p] == "undefined" || primary[p] === null) {
+                            primary[p] = {};
+                        }
+
+                        primary[p] = recursiveObjectMerge( primary[p], overwrite[p] );
+                    }
+                    else{
+                        primary[p] = overwrite[p];
+                    }
+                }
+                catch( e ){
+                    // destination doesn't have that property, create and set it
+                    primary[p] = overwrite[p];
+                }
+            }
+
+            // primary is modified (it's a reference), but pass it back
+            // to keep up the idea that this function returns a result
+            return primary;
+        }
 
         return RouterBase;
     }
