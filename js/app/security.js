37b456c9e88ac112d19f75b3240118c26fc177d1
rockerest <tom@vu>
2014-04-17 17:28:08 -0500

    Add security object to handle log in / out

---
+++ js/app/security.js
@@ -0,0 +1,34 @@
+define(
+    ["cookies", "objects/user"],
+    function( Cookies, User ){
+        var Security = {},
+            User = new User();
+
+        Security.logIn = function( authenticationResponse ){
+            if( authenticationResponse.user ){
+                Cookies( "authentication", JSON.stringify( authenticationResponse.user ) );
+            }
+            else{
+                Cookies.expire( "authentication" );
+            }
+        };
+
+        Security.logOut = function(){
+            Cookies.expire( "authentication" );
+            window.location = "/";
+        };
+
+        Security.userIsAuthenticated = function(){
+            var auth = User.getAuthentication();
+
+            if( auth && auth.id ){
+                return true;
+            }
+            else{
+                return false;
+            }
+        };
+
+        return Security;
+    }
+);
