ed9d4207099a50d344089cf8c97e91bef1c39e3f
rockerest <tom@vu>
2014-04-17 13:53:25 -0500

    make a dummy authentication request

--- js/app/events/login.js
+++ js/app/events/login.js
@@ -1,7 +1,8 @@
 define(
-    ["event", "jquery"],
-    function( Event, $ ){
-        var LoginEvents = function(){};
+    ["event", "jquery", "transfer", "cookies"],
+    function( Event, $, Transfer, Cookies ){
+        var LoginEvents = function(){},
+            Transfer = new Transfer();
 
         LoginEvents.prototype.registerEvents = function( binder ){
             this.binder = binder;
@@ -13,12 +14,21 @@ define(
             this.binder( "rlc.click.login.home.authenticate", function( e ){
                 var clicked = e.target,
                     $clicked = $( clicked ),
-                    data = {
+                    credentials = {
                         "username": $clicked.siblings( '[name="username"]' ).val(),
                         "password": $clicked.siblings( '[name="password"]' ).val()
                     };
 
-                console.info( data );
+                $.when(
+                    Transfer.requestAuthentication( credentials )
+                )
+                .done( function( data, stat, x ){
+                    Cookies( "authentication", 1 );
+                    window.location = "/#/dashboard";
+                })
+                .fail( function( x, stat, t ){
+                    console.log( x, stat, t );
+                });
             });
         };
 
